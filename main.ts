edf0a85d9cda8bf506c916b419bc8c0ec16fd08a
Alexanderdunlop <alexjamesdunlop@gmail.com>
2024-09-27 15:07:06 +0100

    deno project

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
-function greet(name: string): string {
-  return `Hello, ${name}!`;
+export function add(a: number, b: number): number {
+  return a + b;
 }
 
-console.log(greet("world"));
+// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+  console.log("Add 2 + 3 =", add(2, 3));
 
-console.log(greet("bruh"));
+  console.log("Add 5 + 3 =", add(5, 3));
+}
