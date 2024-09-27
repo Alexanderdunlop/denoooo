2236456a475f676fdea1728e7967060adf11a568
Alexanderdunlop <alexjamesdunlop@gmail.com>
2024-09-27 15:56:32 +0100

    reducer

--- main.ts
+++ main.ts
@@ -7,4 +7,17 @@ if (import.meta.main) {
   console.log("Add 2 + 3 =", add(2, 3));
 
   console.log("Add 5 + 3 =", add(5, 3));
+
+  const nums = [1, 2, 3, 4];
+  const fn = function sum(accum: number, curr: number) {
+    return accum + curr;
+  };
+
+  let val = 0;
+  let i = 0;
+  while (i < nums.length) {
+    val = fn(val, nums[i]);
+    i++;
+  }
+  console.log("Sum of array [1,2,3,4] is", val);
 }
